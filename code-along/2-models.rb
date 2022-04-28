6348b3b14939094d21177da47391022ff3b2f9dd
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-04-28 01:13:43 +0000

    alot has changed

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -15,9 +15,43 @@ Company.destroy_all
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
+puts "There are #{Company.all.count} companies"
 
+new_company = Company.new
+# puts new_company.inspect # generates something that looks like a hash
+new_company["name"] = "Apple" 
+new_company["city"] = "Cupertino" 
+new_company["state"] = "CA" 
+new_company["url"] = "https://apple.com" 
+new_company.save
+
+puts "There are #{Company.all.count} companies"
+
+company = Company.new
+company["name"] = "Amazon"
+company["city"] = "Seattle"
+company["state"] = "WA"
+company.save
+
+puts "There are #{Company.all.count} companies"
+
+zebra = Company.new
+zebra["name"] = "Twitter"
+zebra["city"] = "San Franisco"
+zebra["state"] = "CA"
+zebra["url"] = "https://twitter.com"
+zebra.save
+
+puts "What are you? #{new_company.class}"
+
+puts "There are #{Company.all.count} companies"
 # 3. query companies table to find all row with California company
+all_companies = Company.all
+#puts all_companies.inspect
+#put all_companies[0].inspect
 
+cali_companies = Company.where({"state" =>"CA"})
+puts cali_companies.inspect
 # 4. query companies table to find single row for Apple
 
 # 5. read a row's column value
